6959fe737f283f20781b7358403dae782893a994
phant <phant890516@gmail.com>
2025-12-05 14:27:37 +0900

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  preview: {
+    host: true,
+    port: 4173,
+    allowedHosts: ["profile-5e8r.onrender.com"]
+  }
 })
